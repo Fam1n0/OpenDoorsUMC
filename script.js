09e1f6f6c4ff53d5a982ef4d604aa51682e91316
Fam1n0 <142743561+Fam1n0@users.noreply.github.com>
2024-02-23 12:28:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // scripts.js
 document.addEventListener('DOMContentLoaded', function() {
     // This is where you can add JavaScript that will run once the DOM is fully loaded
+    // Currently empty because there are no dynamic features requested yet
 });
